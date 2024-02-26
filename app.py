f03eaa326418b43423cf9ba80bb86a302b9900dc
Adam Kostandy <adamkostandy@gmail.com>
2024-02-26 18:22:57 -0500

    Hello there!

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import sys
 import streamlit as st
 import pandas as pd
-
+import matplotlib.pyplot as plt
 def main():
     chunksize = 10 ** 5 # 100,000 rows at a time
     pr = None
@@ -37,8 +37,9 @@ def main():
     "Financial Plan - Number of Contracts": "int64",
 }
     
-    st.title('Example Data Open NYC Budget Data')
-    st.write('Loading 100,000 rows at a time from the NYC Open Budget Data.')
+    st.title('NYC 2023 Budget Data')
+    st.markdown('Loading 100,000 rows at a time from the NYC Open Budget Data.')
+    
     # Load the CSV data
     try:
         chunksize = 10 ** 5 # 100,000 rows at a time
@@ -61,18 +62,30 @@ def main():
             # Handle other types of errors or incomplete information
             print("Error occurred:", error_message)    
         # Show the data
-    st.write(data)
-  
-    st.write('List of all the Agencies')
+    st.markdown('### Data Overview')
+    st.dataframe(data)
+
+    total_budget = data['Current Modified Budget Amount'].sum()
+    st.markdown(f'### Total budget for fiscal year 2023: {total_budget}')
+
+    st.markdown('### List of all the Agencies')
     st.write(data['Agency Name'].unique())
-    
+
     size = data['Agency Name'].unique().size
-    st.write(size)
+    st.markdown(f'### Number of unique agencies: {size}')
+
+    st.markdown('### Data Editor')
     edited_df = st.data_editor(data)
+
+    budget_per_agency = data.groupby('Agency Name')['Current Modified Budget Amount'].sum()
+    st.markdown('### Budget per Agency')
+    st.bar_chart(budget_per_agency)
     
-    
-    
-    
+    st.markdown('### Frequency of Object Class Names')
+    object_class_counts = data['Object Class Name'].value_counts()
+    fig, ax = plt.subplots()
+    ax.pie(object_class_counts, labels=object_class_counts.index, autopct='%1.1f%%')
+    st.pyplot(fig)    
   
 if __name__ == '__main__':
     main()
