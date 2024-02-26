836c1bf3af26297e1b3030d267ecb393ca11f854
Adam Kostandy <adamkostandy@gmail.com>
2024-02-25 20:46:49 -0500

    Add data loading and display functionality

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ import streamlit as st
 import pandas as pd
 
 def main():
-    st.title('Hello World')
-    st.write('This is a simple example of a Streamlit app.')
-  
+    chunksize = 10 ** 5 # 100,000 rows at a time
+    
+    st.title('Example Data Open NYC Budget Data')
+    st.write('Loading 100,000 rows at a time from the NYC Open Budget Data.')
+    # Load the CSV data
+    data = pd.read_csv('data/Expense_Budget.csv', nrows=chunksize)
+    
+    # Show the data
+    st.write(data)
   
 if __name__ == '__main__':
     main()
